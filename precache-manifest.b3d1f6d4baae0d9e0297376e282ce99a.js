987f8cbda47c7f7608b40c2f9178e050ec8a0f77
shannon <sh4nnongoh@gmail.com>
2020-02-07 23:14:23 +0800

    moved build to home

---
+++ precache-manifest.b3d1f6d4baae0d9e0297376e282ce99a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0db6f5792648f92329066aa8e8370a54",
+    "url": "/loa/index.html"
+  },
+  {
+    "revision": "792f76ca8a8f2722a5d2",
+    "url": "/loa/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e7095d636ef537840f9c",
+    "url": "/loa/static/js/2.6f6d9903.chunk.js"
+  },
+  {
+    "revision": "ac3f8e58b5796b271331f2926a0e8b61",
+    "url": "/loa/static/js/2.6f6d9903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792f76ca8a8f2722a5d2",
+    "url": "/loa/static/js/main.64d49fbb.chunk.js"
+  },
+  {
+    "revision": "4e13a2c75f095ed7282a",
+    "url": "/loa/static/js/runtime-main.28792b3b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/loa/static/media/logo.5d5d9eef.svg"
+  }
+]);

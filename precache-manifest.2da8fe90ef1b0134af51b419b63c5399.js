a3cabcbbd30a93693cad75ad69b752b4c74f461b
Shannon Goh <sh4nnongoh@gmail.com>
2020-02-10 20:33:49 +0800

    test

---
+++ precache-manifest.2da8fe90ef1b0134af51b419b63c5399.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436736c4c6346afc4c9f214da6935a3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1afc841c6d71e7d49317",
+    "url": "/static/css/main.c898e1a1.chunk.css"
+  },
+  {
+    "revision": "c1aff6fbd755acfcf395",
+    "url": "/static/js/2.648b9912.chunk.js"
+  },
+  {
+    "revision": "ac3f8e58b5796b271331f2926a0e8b61",
+    "url": "/static/js/2.648b9912.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1afc841c6d71e7d49317",
+    "url": "/static/js/main.c70560b9.chunk.js"
+  },
+  {
+    "revision": "7d86a45dae03896f601b",
+    "url": "/static/js/runtime-main.7c5c4267.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);

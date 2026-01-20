f8e4c88533010766e3253a0d260411a0de063d5c
Shannon Goh <sh4nnongoh@gmail.com>
2026-01-20 14:06:44 +0800

    enhance SEO

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,18 @@
 export default {
 	title: "Shannon's Software Engineering Blog",
-	url: "https://sh4nnongoh.github.io/",
+	url: "https://sh4nnongoh.github.io",
 	language: "en",
 	description: "I am writing about my experiences as a Software Engineer.",
 	author: {
 		name: "Shannon Goh",
-		email: "sh4nnongoh@gmail.com"
+		email: "sh4nnongoh@gmail.com",
+		url: "https://www.linkedin.com/in/sh4nnongoh",
+		github: "https://github.com/sh4nnongoh",
+		logo: "https://sh4nnongoh.github.io/img/me512.jpeg"
+	},
+	publisher: {
+		name: "Shannon Goh",
+		email: "sh4nnongoh@gmail.com",
+		logo: "https://sh4nnongoh.github.io/img/me512.jpeg"
 	}
 }
